1b1478d74c6ae5c124583a89e61711329386716c
rwayan <iamayan@gmail.com>
2022-05-18 18:13:27 +0800

    Update testpy.py

--- testpy.py
+++ testpy.py
@@ -1,11 +1,11 @@
 import halton
 N = 100000000
-
+DIM = 2
 import numpy as np
 import datetime
 
 start = datetime.datetime.now()
-b = halton.genHalton_all(2,N)
+b = halton.genHalton_all(DIM,N)
 end = datetime.datetime.now()
 print(end - start)
 start = datetime.datetime.now()
@@ -14,7 +14,15 @@ end = datetime.datetime.now()
 print(end - start)
 
 
-
+import halton
+N = 10
+h = halton.halton(True)
+h.init(DIM,True,True)
+for i in range(N):
+    h.genHalton()
+    for j in range(DIM):
+        res = h.get_rnd(j+1)
+        print(res)
 
 import timeit
 
